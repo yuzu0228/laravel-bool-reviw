bbbb7637f66ec82f52d090f7690b4405eb89699e
EC2 Default User <ec2-user@ip-172-31-40-226.ap-northeast-1.compute.internal>
2020-05-05 04:49:53 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'ReviewController@index')->name('index');
+
+Route::group(['middleware' => 'auth'], function() {
+
+Route::get('/review', 'ReviewController@create')->name('create');
+Route::post('/review/store', 'ReviewController@store')->name('store');
+
+});
+
+Route::get('/show/{id}', 'ReviewController@show')->name('show');
